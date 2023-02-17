07ceff051beb3e6cccafa6210981616702893b74
Natnael <nathanielaf01@gmail.com>
2023-02-17 22:19:14 +0300

    print a-z then A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: prints lowercase alphabet
+*             then uppercase alphabet
+*
+* Return: 0(void)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j < 90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
